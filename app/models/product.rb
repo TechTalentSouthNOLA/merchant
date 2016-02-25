a88da522de6c5f7366d34dd558318b736fbba6f3
Sia <siakaramalegos@gmail.com>
2016-02-25 10:17:27 -0600

    Change image to center and crop and adding missing images

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ActiveRecord::Base
   belongs_to :brand
   belongs_to :category
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar,
+    styles: { large: "500x500#", medium: "300x300#", thumb: "100x100#" },
+    default_url: ":style_missing.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
