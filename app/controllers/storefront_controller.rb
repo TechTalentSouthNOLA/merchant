4d891298bf5c49b2142b605060a5d193b8026b12
Sia <siakaramalegos@gmail.com>
2016-03-01 09:46:58 -0600

    Added category dropdown menu to filter products

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,6 +1,11 @@
 class StorefrontController < ApplicationController
   def index
-    @products = Product.all
+    if params[:category_id]
+      @category = Category.find(params[:category_id])
+      @products = Product.where(category: @category)
+    else
+      @products = Product.all
+    end
   end
 
   def about
