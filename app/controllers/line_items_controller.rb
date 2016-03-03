55924e838b012ebb9705e4ceb9cde813cb717007
Sia <siakaramalegos@gmail.com>
2016-03-03 11:44:33 -0600

    Adds remove item from cart button

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -8,10 +8,19 @@ class LineItemsController < ApplicationController
 
     if @line_item.save
       flash[:notice] = "#{product.name} was successfully added to your cart!"
-      redirect_to shop_url
+      redirect_to edit_cart_path(@cart)
     else
       flash.now[:alert] = "Could not add item to your cart."
       redirect_to :back
+    end
+  end
+
+  def destroy
+    @line_item = @cart.line_items.find(params[:id])
+    if @line_item.destroy
+      redirect_to edit_cart_path(@cart), notice: "Item removed from cart."
+    else
+      redirect_to @cart, alert: "Could not remove item from cart."
     end
   end
 end
