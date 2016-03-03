de110eb6824fd3d478c03f4ce9757c08b3007c03
Sia <siakaramalegos@gmail.com>
2016-03-03 08:48:30 -0600

    Fixes multiples bug in shopping cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,7 +4,7 @@ class LineItemsController < ApplicationController
 
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product, price: product.current_price)
+    @line_item = @cart.add_product(product.id)
 
     if @line_item.save
       flash[:notice] = "#{product.name} was successfully added to your cart!"
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,4 +8,19 @@ class Cart < ActiveRecord::Base
     # Ruby version of summing
     # line_items.to_a.sum{ |item| item.total }
   end
+
+  def add_product(product_id)
+    product = Product.find(product_id)
+
+    # Find out if this product is already in cart. If not, current_item will be nil
+    current_item = line_items.find_by(product_id: product_id)
+
+    # If current_item does exist in cart, increase the quantity. If not, build the new line item.
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product: product, price: product.current_price)
+    end
+    current_item
+  end
 end
